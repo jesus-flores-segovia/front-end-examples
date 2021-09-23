40f30922b7d01369754b74fbbd9f8c856009cc8f
Jesús <Jess@LAPTOP-QRSS98AU>
2021-09-23 20:56:48 +0200

    Feature: added sticky navigation bar example

---
+++ 01-Sticky Navigation Bar/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    /* window.onscroll = () => {
+        const navbar = document.getElementById("navbar");
+        var navbar_topOffset = navbar.offsetTop;
+    
+        window.scrollY > navbar_topOffset ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }; */
+});
